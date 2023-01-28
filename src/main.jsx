02613799d74ab9253c80ad83f8b7953d61df411b
KnlnKS <soulkks@hotmail.ca>
2023-01-28 15:59:28 -0500

    🎨 Add background

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import Home from "./pages/Home";
 import App from "./pages/App/App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
